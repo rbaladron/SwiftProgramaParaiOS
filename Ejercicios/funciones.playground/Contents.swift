9dceb8af9770cb3df932be7cd627e9549be058c9
rbaladron <rbaladron@gmail.com>
2015-11-21 19:56:23 +0100

    Tuplas-1

--- Ejercicios/funciones.playground/Contents.swift
+++ Ejercicios/funciones.playground/Contents.swift
@@ -4,13 +4,22 @@ import UIKit
 
 
 
-func calcularIMC(pesoIntegral peso: Double, altura: Double) ->Double{
-
+func calcularIMC(pesoIntegral peso: Double, altura: Double) ->(Double, String){
     
-    return peso / (altura * altura)
+    let imc = peso / (altura * altura)
+    var mensaje = ""
+    if (imc > 18.50 && imc < 25.00){
+        mensaje = "Peso normal"
+    }else{
+        mensaje = "Debes de acudir con tu médico"
+    }
+    
+    let resultado = (imc, mensaje)
+    return resultado
 }
 
-print(calcularIMC(pesoIntegral:95.0, altura:1.78))
-let imc = calcularIMC(pesoIntegral:95.0, altura:1.78)
-print(imc)
+let resultadoIMC = calcularIMC(pesoIntegral:95.0, altura:1.78)
+
+resultadoIMC.0
+resultadoIMC.1
 
